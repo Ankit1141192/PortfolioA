d43592812413f171389c3e8358c700f8b7e516c9
Ankit kumar <ankit2914978@gmail.com>
2026-08-06 15:10:30 +0530

    Update backend API URL in api.js

--- Portfolio/src/config/api.js
+++ Portfolio/src/config/api.js
@@ -1,2 +1,2 @@
-const rawApi = import.meta.env.VITE_BACKEND_API || "https://pink-herring-957072.hostingersite.com";
+const rawApi = import.meta.env.VITE_BACKEND_API || "https://appliedjob1.onrender.com/";
 export const backendApi = rawApi.replace(/\/$/, "");
